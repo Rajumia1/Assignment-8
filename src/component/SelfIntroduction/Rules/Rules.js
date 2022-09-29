fe06bac8c5412081936f4a10cf0f4c3ef6f7ad1d
Rajumia1 <rajuahmedbabu61@gmail.com>
2022-09-29 12:20:40 +0600

    onclick added

--- src/component/SelfIntroduction/Rules/Rules.js
+++ src/component/SelfIntroduction/Rules/Rules.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Rules.css';
 const Rules = ({ items }) => {
+    const [times, setTimes] = useState(0);
+
+    const addBreack = () => {
+        const newTime = times;
+        setTimes(newTime)
+    }
 
     let total = 0;
     for (const item of items) {
@@ -13,15 +19,15 @@ const Rules = ({ items }) => {
             <h1>Vai Ami to obak:{items.length}</h1>
             <h1>Add A Break</h1>
             <div className='duration'>
+                <button className='border-radius ' onClick={() => addBreack(20)}>20s</button>
+                <button className='border-radius'>30s</button>
+                <button className='border-radius'>40s</button>
+                <button className='border-radius'>50s</button>
 
-                <div className='border-radius'><p>20s</p></div>
-                <div className='border-radius'> <p>30s</p></div>
-                <div className='border-radius'><p>40s</p></div>
-                <div className='border-radius'><p>50s</p></div>
             </div>
             <h1>Exercise Details</h1>
             <div className='exercise-detail'> <h3>Exercise time: {total} <small>seconds</small></h3></div>
-            <div className='exercise-detail'> <h3>Break time: <span>00</span> seconds</h3></div>
+            <div className='exercise-detail'> <h3>Break time:  {times} seconds</h3></div>
 
             <button className='btn-activity'><p>Activity Completed</p></button>
         </div>
